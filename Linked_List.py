0d0298de4e9880edf0f9354e3372c2e8962f4ff2
natanaelolanda <ntolanda@gmail.com>
2020-09-07 18:59:04 -0300

    Added lenght, insert, delete and position methods

--- Linked_List.py
+++ Linked_List.py
@@ -4,7 +4,7 @@ class Element:
         self.next = None
 
 class LinkedList:
-    def __init__(self, head=None):
+    def __init__(self, head=None):   
         self.head = head
 
     def __repr__(self):
@@ -24,17 +24,72 @@ class LinkedList:
             current.next = new_element
         else:
             self.head = new_element
+    
+    def get_position(self, position):
+        counter = 1
+        current = self.head
+        if position < 1:
+            return None
+        while current and counter <= position:
+            if counter == position:
+                return current
+            current = current.next
+            counter += 1
+        return None
+    
+    def get_lenght(self):
+        counter = 0
+        current = self.head
+        while current:
+            counter += 1
+            current = current.next
+        return counter
 
+    def insert(self, new_element, position):
+        counter = 1
+        current = self.head
+        if position > 1:
+            while current and counter < position:
+                if counter == position - 1:
+                    new_element.next = current.next
+                    current.next = new_element
+                current = current.next
+                counter += 1
+        elif position == 1:
+            new_element.next = self.head
+            self.head = new_element
+
+    def delete(self, value):
+        current = self.head
+        previous = None
+        while current.value != value and current.next:
+            previous = current
+            current = current.next
+        if current.value == value:
+            if previous:
+                previous.next = current.next
+            else:
+                self.head = current.next
+        
+  
         
 element1 = Element("a")
 element2 = Element("b")
 element3 = Element(4)
 element4 = Element(47)
+element5 = Element("oi")
 
-linkedList = LinkedList(element1)
+ll = LinkedList(element1)
 
-linkedList.append(element2)
-linkedList.append(element3)
-linkedList.append(element4)
+ll.append(element2)
+ll.append(element3)
+ll.append(element4)
+ll.append(element5)
 
-print(linkedList)
+print(ll)
+#print(ll.get_position(4).value)
+print(ll.get_lenght())
+ll.insert(Element('fr'), 3)
+print(ll)
+ll.delete(4)
+print(ll)
